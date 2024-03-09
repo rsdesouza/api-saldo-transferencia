7056560d725d61492fa41fc6851281cf651b6c15
Rodolfo Silva de Souza <sdesouza.rodolfo@gmail.com>
2024-03-08 22:30:09 -0300

    feat: configs inicias do projeto

---
+++ src/main/java/br/com/bank/apisaldotransferencia/ApiSaldoTransferenciaApplication.java
@@ -0,0 +1,13 @@
+package br.com.bank.apisaldotransferencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSaldoTransferenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSaldoTransferenciaApplication.class, args);
+	}
+
+}
