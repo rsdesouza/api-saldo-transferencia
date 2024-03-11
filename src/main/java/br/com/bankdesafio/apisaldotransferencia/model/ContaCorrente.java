4265383c6f2b6bbc8066b6da1075a5395d146821
Rodolfo Silva de Souza <sdesouza.rodolfo@gmail.com>
2024-03-11 08:56:43 -0300

    feat: adicionando atributos das entidades principais

--- src/main/java/br/com/bankdesafio/apisaldotransferencia/model/ContaCorrente.java
+++ src/main/java/br/com/bankdesafio/apisaldotransferencia/model/ContaCorrente.java
@@ -1,4 +1,33 @@
 package br.com.bankdesafio.apisaldotransferencia.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tbl_conta_corrente")
+@Data
+@AllArgsConstructor
 public class ContaCorrente {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "nome_cliente")
+    private String nomeCliente;
+
+    @Column(name = "saldo")
+    private BigDecimal saldo;
+
+    @Column(name = "ativa")
+    private Boolean ativa;
+
+
 }
--- src/main/java/br/com/bankdesafio/apisaldotransferencia/model/Transacao.java
+++ src/main/java/br/com/bankdesafio/apisaldotransferencia/model/Transacao.java
@@ -1,4 +1,39 @@
 package br.com.bankdesafio.apisaldotransferencia.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.UUID;
+import jakarta.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@Table(name = "tbl_transacao")
 public class Transacao {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "valor")
+    private BigDecimal valor;
+
+    @Column(name = "data_transacao")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataTransacao;
+
+    @ManyToOne
+    @JoinColumn(name = "conta_origem_id", referencedColumnName = "id")
+    private ContaCorrente contaOrigem;
+
+    @ManyToOne
+    @JoinColumn(name = "conta_destino_id", referencedColumnName = "id")
+    private ContaCorrente contaDestino;
+
 }
