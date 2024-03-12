385c40ee92dc39b32b7e7f9f64d37e9b1524be20
Rodolfo Silva de Souza <sdesouza.rodolfo@gmail.com>
2024-03-12 16:51:13 -0300

    feat: adicionado configurações de resiliencias da pp

--- src/main/java/br/com/bankdesafio/apisaldotransferencia/config/Resilience4jConfig.java
+++ src/main/java/br/com/bankdesafio/apisaldotransferencia/config/Resilience4jConfig.java
@@ -1,4 +1,32 @@
 package br.com.bankdesafio.apisaldotransferencia.config;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
 public class Resilience4jConfig {
+
+    @Bean
+    public CircuitBreakerRegistry circuitBreakerRegistry() {
+        CircuitBreakerConfig config = CircuitBreakerConfig.custom()
+                .failureRateThreshold(50)
+                .waitDurationInOpenState(Duration.ofSeconds(10))
+                .slidingWindowSize(20)
+                .build();
+        return CircuitBreakerRegistry.of(config);
+    }
+
+    @Bean
+    public TimeLimiterRegistry timeLimiterRegistry() {
+        TimeLimiterConfig config = TimeLimiterConfig.custom()
+                .timeoutDuration(Duration.ofSeconds(5))
+                .build();
+        return TimeLimiterRegistry.of(config);
+    }
 }
