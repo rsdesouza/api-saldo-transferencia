d30a75fab1ae41782ae399e63c377a4d544a7241
Rodolfo Silva de Souza <sdesouza.rodolfo@gmail.com>
2024-03-19 07:42:35 -0300

    feat: ajustes para suportar alta carga

--- src/main/java/br/com/bankdesafio/apisaldotransferencia/config/DataSourceConfig.java
+++ src/main/java/br/com/bankdesafio/apisaldotransferencia/config/DataSourceConfig.java
@@ -25,16 +25,24 @@ public class DataSourceConfig {
         dataSource.setUsername(dbUsername);
         dataSource.setPassword(dbPassword);
 
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver"); // Para MySQL 8
-        dataSource.setMaxActive(10);
-        dataSource.setMinIdle(5);
-        dataSource.setMaxIdle(10);
-        dataSource.setInitialSize(5);
-        dataSource.setMaxWait(10000);
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+
+        // Ajustes para suportar alta carga
+        dataSource.setMaxActive(6000);
+        dataSource.setMinIdle(100);
+        dataSource.setMaxIdle(1000);
+        dataSource.setInitialSize(100);
+        dataSource.setMaxWait(5000);
         dataSource.setValidationQuery("SELECT 1");
         dataSource.setTestOnBorrow(true);
-        dataSource.setMinEvictableIdleTimeMillis(60000);
-        dataSource.setTimeBetweenEvictionRunsMillis(5000);
+        dataSource.setMinEvictableIdleTimeMillis(30000);
+        dataSource.setTimeBetweenEvictionRunsMillis(15000);
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTestOnReturn(false);
+        dataSource.setJdbcInterceptors(
+                "org.apache.tomcat.jdbc.pool.interceptor.ConnectionState;"+
+                        "org.apache.tomcat.jdbc.pool.interceptor.StatementFinalizer"
+        ); // Interceptors para otimizar o pool
 
         return dataSource;
     }
